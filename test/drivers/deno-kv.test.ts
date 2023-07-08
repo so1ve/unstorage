cd61b8b8b879ffc6b77ced7ca7910f5ec14df4bc
nakasyou <syoutarou1103@outlook.jp>
2023-07-08 18:29:43 +0900

    Remove not using import

--- test/drivers/deno-kv.test.ts
+++ test/drivers/deno-kv.test.ts
@@ -1,25 +1,21 @@
 import { describe, it, expect, vi } from "vitest";
 import { execa, execaCommandSync } from "execa";
 import { readFile, mkdir, writeFile } from "fs/promises";
-import { Buffer } from "buffer";
 import crypto from "crypto"
 
 const hasDeno =
   execaCommandSync("deno --version", { stdio: "ignore", reject: false })
     .exitCode === 0;
 
-
 try{
   await mkdir("./tmp") // Make tmp dir
 } catch(_e) {} // Dir exists
 
 describe.runIf(hasDeno)("drivers: deno-kv", async () => {
-
-
   const driverModuleCode = (await readFile("./src/drivers/deno-kv.ts")).toString().replace(/(?<=from ")\.\/utils\/.*?(?=")/g, str => {
     return "../src/drivers/"+str+".ts"
-  })
-  await writeFile("./tmp/deno-kv-for-deno.ts", driverModuleCode)
+  });
+  await writeFile("./tmp/deno-kv-for-deno.ts", driverModuleCode);
 
   const driverInitCode = `
   import driver from './tmp/deno-kv-for-deno.ts';
@@ -42,5 +38,5 @@ describe.runIf(hasDeno)("drivers: deno-kv", async () => {
       code,
       "--unstable", // If Deno use Kv, we must give "--unstable" flag to Deno.
     ])).stdout).toBe(value)
-  }, 100000)
+  }, 100000);
 })
